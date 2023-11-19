ba3832dbfad5163ad1a4654dc7a3f4004306901b
Muhammad Ahsan <ahsan.dev.404@gmail.com>
2023-11-20 00:10:50 +0500

    basic setup done

---
+++ Index.jsx
@@ -0,0 +1,11 @@
+import { View, Text, StatusBar } from 'react-native'
+import React from 'react'
+
+export default function Index() {
+  return (
+    <>
+    <StatusBar />
+      <Text>Index</Text>
+    </>
+  )
+}
