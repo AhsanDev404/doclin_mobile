561f9bb2f383b60b4c24131699b358d433d4d1d3
Muhammad Ahsan <ahsan.dev.404@gmail.com>
2023-11-20 03:28:33 +0500

    navigation setup complete

--- src/layouts/MedicalCardNavigation.jsx
+++ src/layouts/MedicalCardNavigation.jsx
@@ -1,10 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import MedicalCard from "../screens/medicalCard/Medicalcard";
+import MedicalCard from "../screens/medicalCard/MedicalCard";
 import Recommendation from "../screens/medicalCard/Recommendation";
 import Details from "../screens/medicalCard/Details";
 import Appointment from "../screens/medicalCard/Appointment";
+import TimeSlots from "../screens/medicalCard/TimeSlots";
+import Confirm from "../screens/medicalCard/Confirm";
 
 const Stack = createNativeStackNavigator();
 export default function MedicalCardNavigation() {
@@ -15,6 +17,8 @@ export default function MedicalCardNavigation() {
         <Stack.Screen name="Recommendation" component={Recommendation} />
         <Stack.Screen name="Details" component={Details} />
         <Stack.Screen name="Appointment" component={Appointment} />
+        <Stack.Screen name="TimeSlots" component={TimeSlots} />
+        <Stack.Screen name="Confirm" component={Confirm} />
       </Stack.Navigator>
     </>
   );
---
+++ src/screens/medicalCard/TimeSlots.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function TimeSlots() {
+  return (
+    <View>
+      <Text>TimeSlots</Text>
+    </View>
+  )
+}
