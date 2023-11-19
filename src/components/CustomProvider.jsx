bba01fc5ee060271e139e7e477a37d34d8854a97
Muhammad Ahsan <ahsan.dev.404@gmail.com>
2023-11-20 03:32:33 +0500

    redux setup complete

--- src/components/CustomProvider.jsx
+++ src/components/CustomProvider.jsx
@@ -1,12 +1,14 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { PaperProvider } from "react-native-paper";
+import { Provider } from "react-redux";
+import store from "../redux/store";
 export default function CustomProvider({ children }) {
   return (
     <>
       <PaperProvider>
-        {children}
-        </PaperProvider>
+        <Provider store={store}>{children}</Provider>
+      </PaperProvider>
     </>
   );
 }
